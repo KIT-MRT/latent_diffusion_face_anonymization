8f3844ca77e5a66860d7bea072b673300743261e
roesch <kevin.roesch@kit.edu>
2024-05-16 11:56:36 +0200

    move for loop content to own function

--- scripts/face_anonymization.py
+++ scripts/face_anonymization.py
@@ -1,13 +1,13 @@
-from PIL import Image
-import numpy as np
-from pathlib import Path
 import os
 from tqdm import tqdm
 import logging
 
 import diffusion_face_anonymisation.utils as dfa_utils
 from diffusion_face_anonymisation.io_functions import setup_parser_and_parse_args
-from diffusion_face_anonymisation.anonymization_functions import define_anon_function
+from diffusion_face_anonymisation.anonymization_functions import (
+    define_anon_function,
+    anonymize_image,
+)
 
 # Set up logging
 logging.basicConfig(
@@ -37,40 +37,4 @@ if __name__ == "__main__":
         image_file = entry["image_file"]
         mask_file = entry["mask_file"]
         logger.info(f"Processing image {image_file} with mask {mask_file}")
-
-        image = Image.open(image_file)
-        debug_img = np.array(image)
-
-        all_faces_bb_list = dfa_utils.get_face_bounding_box_list_from_file(mask_file)
-        mask_dict_list = dfa_utils.convert_bb_to_mask_dict_list(
-            all_faces_bb_list, image_width=image.width, image_height=image.height
-        )
-        logger.debug(
-            f"Found {len(mask_dict_list)} faces in image {Path(image_file).stem}"
-        )
-
-        inpainted_img_list = [
-            anon_function(image=image, mask=mask_dict["bb"])  # type: ignore
-            for mask_dict in mask_dict_list
-        ]
-
-        final_img = dfa_utils.add_inpainted_faces_to_orig_img(
-            image, inpainted_img_list, mask_dict_list
-        )
-
-        orig_file = Path(image_file)
-        # Construct paths for output and debug images
-        output_filename = f"{orig_file.stem}_anon_{anon_function}{orig_file.suffix}"
-        debug_img_filename = (
-            f"debug_{orig_file.stem}_anon_{anon_function}{orig_file.suffix}"
-        )
-
-        output_path = output_dir / output_filename
-        debug_output_path = output_dir / "debug" / debug_img_filename
-
-        # Save the final image and debug image
-        final_img.save(output_path)
-        Image.fromarray(debug_img).save(debug_output_path)
-
-        logger.info(f"Anonymized image saved to {output_path}")
-        logger.debug(f"Debug image saved to {debug_output_path}")
+        anonymize_image(image_file, mask_file, anon_function)
--- src/diffusion_face_anonymisation/anonymization_functions.py
+++ src/diffusion_face_anonymisation/anonymization_functions.py
@@ -1,19 +1,22 @@
 import numpy as np
 from PIL import Image
+from pathlib import Path
 from skimage.filters import gaussian
 from typing import Callable
+import logging
 
 from diffusion_face_anonymisation.utils import FaceBoundingBox
+import diffusion_face_anonymisation.utils as dfa_utils
 
 
-def define_anon_function(anon_method: str) -> Callable | None:
+def define_anon_function(anon_method: str) -> Callable:
     anon_functions = {
         "white": anonymize_face_white,
         "gauss": anonymize_face_gauss,
         "pixel": anonymize_face_pixelize,
         "ldfa": None,  # TODO: implement new ldfa function
     }
-    return anon_functions.get(anon_method)
+    return anon_functions.get(anon_method)  # type: ignore
 
 
 def anonymize_face_white(*, image: np.ndarray, mask: FaceBoundingBox):
@@ -47,3 +50,41 @@ def anonymize_face_pixelize(*, image, pixels_per_block=8):
             ] = mean
 
     return Image.fromarray(img_anon)
+
+
+def anonymize_image(image_file: Path, mask_file: Path, anon_function: Callable):
+
+    image = Image.open(image_file)
+    debug_img = np.array(image)
+
+    all_faces_bb_list = dfa_utils.get_face_bounding_box_list_from_file(mask_file)
+    mask_dict_list = dfa_utils.convert_bb_to_mask_dict_list(
+        all_faces_bb_list, image_width=image.width, image_height=image.height
+    )
+    logging.debug(f"Found {len(mask_dict_list)} faces in image {Path(image_file).stem}")
+
+    inpainted_img_list = [
+        anon_function(image=image, mask=mask_dict["bb"])  # type: ignore
+        for mask_dict in mask_dict_list
+    ]
+
+    final_img = dfa_utils.add_inpainted_faces_to_orig_img(
+        image, inpainted_img_list, mask_dict_list
+    )
+
+    orig_file = Path(image_file)
+    # Construct paths for output and debug images
+    output_filename = f"{orig_file.stem}_anon_{anon_function}{orig_file.suffix}"
+    debug_img_filename = (
+        f"debug_{orig_file.stem}_anon_{anon_function}{orig_file.suffix}"
+    )
+
+    output_path = output_dir / output_filename
+    debug_output_path = output_dir / "debug" / debug_img_filename
+
+    # Save the final image and debug image
+    final_img.save(output_path)
+    Image.fromarray(debug_img).save(debug_output_path)
+
+    logging.info(f"Anonymized image saved to {output_path}")
+    logging.debug(f"Debug image saved to {debug_output_path}")
