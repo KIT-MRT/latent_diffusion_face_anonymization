266289379b883ef5efbb5c71fd4076fc70ce6c26
roesch <kevin.roesch@kit.edu>
2024-06-19 13:28:40 +0200

    adapt test

--- test/mask_handling_test.py
+++ test/mask_handling_test.py
@@ -1,14 +1,17 @@
-import copy
 import unittest
 import os
-import numpy as np
-from PIL import Image
+import logging
+from pathlib import Path
 import diffusion_face_anonymisation.utils as dfa_utils
 import diffusion_face_anonymisation.io_functions as dfa_io
 
-# we only need this for testing because in deployment we don't need the numpy functionality
-def convert_pil_to_np(image):
-    return np.array(image)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("test_ldfa.log"), logging.StreamHandler()],
+)
+
+logger = logging.getLogger(__name__)
 
 
 class TestSingleMaskHandling(unittest.TestCase):
@@ -26,25 +29,13 @@ class TestSingleMaskHandling(unittest.TestCase):
         image_mask_dict = dfa_utils.add_file_paths_to_image_mask_dict(
             png_files, image_mask_dict, "image_file"
         )
-        # we could skip the dfa_utils in the tests to prevent going pil->numpy->pil but this way we actually test the functions we later use in the scripts
+
         for i, entry in enumerate(image_mask_dict.values()):
             image = dfa_utils.preprocess_image(entry["image_file"])
-            image = convert_pil_to_np(image)
-            all_faces_bb_list = dfa_utils.get_face_bounding_box_list_from_file(
-                entry["mask_file"]
-            )
-
-            mask_image_list = dfa_utils.convert_bb_to_mask_image_list(
-                all_faces_bb_list, image.shape[1], image.shape[0]
-            )
-            for j, mask_image_dict in enumerate(mask_image_list):
-                # apply mask to image
-                mask_area = np.where(mask_image_dict["mask"] == 255)
-                image_mask = copy.deepcopy(image)
-                image_mask[:, :, :] = (0, 0, 0)
-                image_mask[mask_area] = (255, 255, 255)
-                image_pil = Image.fromarray(image_mask)
-                image_pil.save(f"/tmp/dfa_mask_test_{i}_{j}.png")
+            faces = dfa_utils.get_faces_from_file(entry["mask_file"])
+            dfa_utils.add_face_cutout_and_mask_img(faces, image)
+            for j, face in enumerate(faces):
+                face.save(Path("/storage_local/roesch/ldba/faces_tmp"), i, j)
 
 
 if __name__ == "__main__":
--- test/sd_face_inpainting_test.py
+++ test/sd_face_inpainting_test.py
@@ -1,19 +1,53 @@
 import unittest
+import numpy as np
+import logging
 import os
+import torch
 from pathlib import Path
 
 import diffusion_face_anonymisation.utils as dfa_utils
 import diffusion_face_anonymisation.io_functions as dfa_io
-from diffusion_face_anonymisation.anonymization_functions import (
-    anonymize_image,
-    define_anon_function,
+
+from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion_inpaint import (
+    StableDiffusionInpaintPipeline,
 )
+from PIL import Image
+
+
+class SDPipelineWrapper:
+    INFERENCE_STEPS = 50
+
+    def __init__(self, prompt: str, gpu_id=0):
+        self.pipe = StableDiffusionInpaintPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-2-inpainting", torch_dtype=torch.float16
+        ).to(torch.device(f"cuda:{gpu_id}"))
+        self.prompt = prompt
+
+    def run(self, image: Image.Image, mask: Image.Image):
+        return self.pipe(
+            prompt=self.prompt,
+            image=image,
+            mask_image=mask,
+            strength=0.75,
+            num_inference_steps=self.INFERENCE_STEPS,
+            generator=torch.Generator("cuda").manual_seed(2353552617),
+        )
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("test_ldfa.log"), logging.StreamHandler()],
+)
+
+logger = logging.getLogger(__name__)
 
 
 class FaceInpaintingTest(unittest.TestCase):
     file_dir = os.path.dirname(os.path.realpath(__file__))
     test_image_base_path = os.path.abspath(os.path.join(file_dir, "leftImg8Bit"))
     test_mask_base_path = os.path.abspath(os.path.join(file_dir, "annotations"))
+    sd_pipe = SDPipelineWrapper("", gpu_id=0)
 
     def test_(self):
         json_files = dfa_io.glob_files_by_extension(self.test_mask_base_path, "json")
@@ -25,13 +59,27 @@ class FaceInpaintingTest(unittest.TestCase):
         image_mask_dict = dfa_utils.add_file_paths_to_image_mask_dict(
             png_files, image_mask_dict, "image_file"
         )
-        anon_function = define_anon_function("ldfa")
 
-        for entry in image_mask_dict.values():
-            image_file = entry["image_file"]
-            mask_file = entry["mask_file"]
-            anon_img = anonymize_image(image_file, mask_file, anon_function)
-            dfa_io.save_anon_image(anon_img, image_file, Path("/tmp"), "ldfa")
+        for i, entry in enumerate(image_mask_dict.values()):
+            image = dfa_utils.preprocess_image(entry["image_file"])
+            faces = dfa_utils.get_faces_from_file(entry["mask_file"])
+            dfa_utils.add_face_cutout_and_mask_img(faces, image)
+            ldfa_image = np.array(image)
+            for j, face in enumerate(faces):
+                face.resize(width=512, height=512)
+
+                face.mask_image_resized = self.sd_pipe.pipe.mask_processor.blur(
+                    face.mask_image_resized, blur_factor=3
+                )
+                anon_image = self.sd_pipe.run(
+                    face.face_image_resized, face.mask_image_resized
+                )
+                face.anon_face = anon_image.images[0]
+                ldfa_image = face.add_to_image(ldfa_image)
+                face.save(Path("/storage_local/roesch/ldba/faces_tmp"), i, j)
+            Image.fromarray(ldfa_image).save(
+                f"/storage_local/roesch/ldba/faces_tmp/anon_{i}.png"
+            )
 
 
 if __name__ == "__main__":
