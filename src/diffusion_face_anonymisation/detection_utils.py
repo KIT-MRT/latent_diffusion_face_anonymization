d0af7702a0dbd484da8752a1ff56d6f9dc98df77
Pradeep-Gopi-E <pradeepgopi99@gmail.com>
2024-09-27 11:22:57 +0200

    merged class body detector

--- src/diffusion_face_anonymisation/detection_utils.py
+++ src/diffusion_face_anonymisation/detection_utils.py
@@ -2,6 +2,11 @@ import logging
 import os
 from tqdm import tqdm
 from pathlib import Path
+import cv2
+import numpy as np
+from ultralytics import YOLO
+import torch
+
 
 import json
 
@@ -51,3 +56,43 @@ def detect_faces_in_files(image_files: list[Path], image_dir: Path, output_dir: 
         logging.info(f"Output written to file: {output_file}")
         with open(output_file, "w+", encoding="utf8") as json_file:
             json.dump({"face": faces}, json_file)
+
+class BodyDetector:
+    def __init__(self):
+        self.model = YOLO("yolov8x-seg.pt")
+    
+    def detect(self, img_file):
+        if not os.path.exists(img_file):
+            raise FileNotFoundError(f"Image file '{img_file}' not found.")      
+        img_bgr = cv2.imread(img_file)
+        if img_bgr is None:
+            raise RuntimeError(f"Failed to read image file '{img_file}'.")
+        
+      
+        img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+        img_height, img_width, _ = img_rgb.shape
+
+        
+        results = self.model(img_rgb,retina_masks=True)
+        
+   
+        persons_cutout = []
+        persons_white_mask = []
+        person_class_index = 0  
+        
+        for result in results:
+            boxes = result.boxes
+            masks = result.masks
+            cls = boxes.cls.tolist()
+        
+            for i in range(len(masks)):
+             
+                if int(cls[i]) == person_class_index:
+                    mask = masks[i].data.cpu().numpy()[0]
+                    person_pixel = np.where(mask == 1)
+                    cutout_img = np.full(img_rgb.shape, (255, 255, 255), dtype=img_rgb.dtype)
+                    cutout_img[person_pixel] = img_rgb[person_pixel]
+                    persons_cutout.append(cutout_img)
+                    persons_white_mask.append(mask)
+        
+        return persons_cutout, persons_white_mask
