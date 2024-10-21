bc8bed108ee117e4cb9091e5b4cfcf81f16f55fe
Pradeep-Gopi-E <pradeepgopi99@gmail.com>
2024-10-21 16:56:46 +0200

    Update anonymization_functions.py
    Added naive body anonymization functions

--- src/diffusion_face_anonymisation/anonymization_functions.py
+++ src/diffusion_face_anonymisation/anonymization_functions.py
@@ -75,3 +75,72 @@ def anonymize_image(image_file: Path, mask_file: Path, anon_function: Callable):
         final_img = face.add_anon_face_to_image(final_img)
 
     return Image.fromarray(final_img)
+
+#Body anonymization Functions
+
+from diffusion_face_anonymisation.body import Body, add_body_cutout_and_mask_img
+from diffusion_face_anonymisation.io_functions import get_bodies_from_file
+
+
+def define_body_anon_function(anon_method: str) -> Callable:
+    anon_functions = {
+        "white": anonymize_body_white,
+        "gauss": anonymize_body_gauss,
+        "pixel": anonymize_body_pixelize,
+        "ldfa": anonymize_body_ldfa,
+    }
+    return anon_functions.get(anon_method)
+
+
+def anonymize_body_white(*, body: Body) -> Body:
+    body.body_anon = Image.fromarray(np.ones_like(np.array(body.body_cutout)) * 255)
+    return body
+
+
+def anonymize_body_gauss(*, body: Body) -> Body:
+    body.body_anon = Image.fromarray(
+        (gaussian(np.array(body.body_cutout), sigma=3, channel_axis=-1) * 255).astype(np.uint8)
+    )
+    return body
+
+
+def anonymize_body_pixelize(*, body: Body) -> Body:
+    pixels_per_block = 8
+    body_img = np.array(body.body_cutout.copy())
+
+    for idx_v in range(body_img.shape[0] // pixels_per_block):
+        for idx_u in range(body_img.shape[1] // pixels_per_block):
+            block = body_img[
+                idx_v * pixels_per_block : (idx_v + 1) * pixels_per_block,
+                idx_u * pixels_per_block : (idx_u + 1) * pixels_per_block,
+            ]
+            mean = np.mean(np.reshape(block, [pixels_per_block * pixels_per_block, 3]), axis=0)
+            body_img[
+                idx_v * pixels_per_block : (idx_v + 1) * pixels_per_block,
+                idx_u * pixels_per_block : (idx_u + 1) * pixels_per_block,
+            ] = mean
+
+    body.body_anon = Image.fromarray(body_img)
+    return body
+
+
+def anonymize_body_ldfa(*, body: Body) -> Body:
+    return body
+
+
+
+def anonymize_body_image(image_file: Path, mask_file: Path, anon_function: Callable) -> Image.Image:
+    image = Image.open(image_file)
+    final_image = np.array(image)
+    bodies = get_bodies_from_file(mask_file)
+    logging.debug(f"Body masks loaded from {mask_file}, total bodies found: {len(bodies)}")
+    bodies = add_body_cutout_and_mask_img(bodies, final_image)
+
+    for body in bodies:
+
+        body = anon_function(body=body)
+        final_image = body.add_anon_body_to_image(final_image)  
+
+    return Image.fromarray(final_image)
+
+
