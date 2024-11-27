f7aeeaf98c6338a04154e1765d066f735d61965a
Pradeep Gopi <pradeepgopi99@gmail.com>
2024-11-27 13:26:13 +0100

    Updated body_mask_detection_test.py

--- test/body_mask_detection_test.py
+++ test/body_mask_detection_test.py
@@ -4,7 +4,7 @@ import logging
 from pathlib import Path
 from diffusion_face_anonymisation import utils
 import diffusion_face_anonymisation.io_functions as dfa_io
-from diffusion_face_anonymisation.detection_utils import BodyDetector as dfa_bd
+from diffusion_face_anonymisation.detection_utils import BodyDetector 
 
 logging.basicConfig(
     level=logging.INFO,
@@ -17,12 +17,12 @@ logger = logging.getLogger(__name__)
 class BodyDetectionTest(unittest.TestCase):
     file_dir = os.path.dirname(os.path.realpath(__file__))
     test_image_base_path = os.path.abspath(os.path.join(file_dir, "leftImg8Bit"))
-    test_mask_base_path = os.path.abspath(os.path.join(file_dir, "Annotation_mask"))
 
     def test_body_detection(self):
+        bd = BodyDetector()
         png_files = dfa_io.glob_files_by_extension(self.test_image_base_path, "png")
         image_files = [Path(os.path.join(self.test_image_base_path, img)) for img in png_files]
-        dfa_bd.body_detect_in_files(image_files=image_files, output_dir=Path("/tmp"))  
+        bd.body_detect_in_files(image_files=image_files, output_dir=Path("/tmp"))  
 
         logging.info(f"Body detection test completed for {len(image_files)} images")
 
