4cf0442743f3489de5d185c1a4a2dd2f490a28a0
roesch <kevin.roesch@kit.edu>
2024-12-03 15:48:08 +0000

    rename test

--- test/simple_face_anon_test.py
+++ test/simple_face_anon_test.py
@@ -5,7 +5,7 @@ import logging
 from pathlib import Path
 from diffusion_face_anonymisation.anonymization_functions import (
     define_anon_function,
-    anonymize_image,
+    anonymize_face_image,
 )
 import diffusion_face_anonymisation.utils as dfa_utils
 import diffusion_face_anonymisation.io_functions as dfa_io
@@ -19,7 +19,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-class BaseTestAnon(unittest.TestCase, ABC):
+class BaseTestFaceAnon(unittest.TestCase, ABC):
     file_dir = os.path.dirname(os.path.realpath(__file__))
     test_image_base_path = os.path.abspath(os.path.join(file_dir, "leftImg8Bit"))
     test_mask_base_path = os.path.abspath(os.path.join(file_dir, "annotations"))
@@ -41,27 +41,27 @@ class BaseTestAnon(unittest.TestCase, ABC):
             image_file = entry["image_file"]
             mask_file = entry["mask_file"]
             logger.info(f"Processing image {image_file} with mask {mask_file}")
-            anon_img = anonymize_image(image_file, mask_file, self.anon_function)
+            anon_img = anonymize_face_image(image_file, mask_file, self.anon_function)
             dfa_io.save_anon_image(
                 anon_img, image_file, self.output_dir, self.anon_type
             )
 
 
-class TestMaskAnon(BaseTestAnon):
+class TestFaceMaskAnon(BaseTestFaceAnon):
     anon_type = "white"
 
     def test_mask_anon(self):
         self.run_test()
 
 
-class TestPixelAnon(BaseTestAnon):
+class TestFacePixelAnon(BaseTestFaceAnon):
     anon_type = "pixel"
 
     def test_pixel_anon(self):
         self.run_test()
 
 
-class TestGaussAnon(BaseTestAnon):
+class TestFaceGaussAnon(BaseTestFaceAnon):
     anon_type = "gauss"
 
     def test_gauss_anon(self):
