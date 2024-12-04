d59b9df63420077aebd13129b6278e32e1409743
roesch <kevin.roesch@kit.edu>
2024-12-04 16:49:24 +0000

    adapt detection for files

--- src/diffusion_face_anonymisation/body_detection.py
+++ src/diffusion_face_anonymisation/body_detection.py
@@ -2,8 +2,7 @@ import cv2
 from ultralytics import YOLO
 import logging
 from pathlib import Path
-from tqdm import tqdm
-import json
+import numpy as np
 
 from diffusion_face_anonymisation.body import Body
 
@@ -31,9 +30,9 @@ class BodyDetector:
             masks = result.masks
             cls = boxes.cls.tolist()
 
-            for object_class_index, mask in zip(masks, cls):
+            for mask, object_class_index in zip(masks, cls):
                 if object_class_index == person_class_index:
-                    mask = mask.data.cpu().numpy()[0]
+                    mask = mask.data.cpu().numpy().astype(np.uint8)[0]
                     body = Body(mask)
                     body.set_body_cutout(img_rgb)
                     body_list.append(body)
@@ -41,19 +40,11 @@ class BodyDetector:
         logging.info(f"Detected {len(body_list)} person(s) in image: {img_file}")
         return body_list
 
-    def body_detect_in_files(self, image_files: list[Path], output_dir: Path):
-        logging.info(f"Starting body detection for {len(image_files)} images.")
-        for img_file in tqdm(image_files):
-            try:
-                logging.info(f"Processing image: {img_file}")
-                body_list = self.body_detect_in_image(img_file)
-
-                output_file = output_dir / f"{img_file.stem}_bodies.json"
-                with open(output_file, "w+", encoding="utf8") as json_file:
-                    json.dump(
-                        {"masks": [body.body_mask.tolist() for body in body_list]},
-                        json_file,
-                    )
-                logging.info(f"Results saved to: {output_file}")
-            except Exception as e:
-                logging.error(f"Error processing image {img_file}: {e}")
+    def body_detect_in_files(self, img_file: Path) -> list[Body]:
+        body_list = []
+        try:
+            logging.info(f"Processing image: {img_file}")
+            body_list = self.body_detect_in_image(img_file)
+        except Exception as e:
+            logging.error(f"Error processing image {img_file}: {e}")
+        return body_list
