ea137410c6e66aec06c6e4177be70ae1adc2579f
roesch <kevin.roesch@kit.edu>
2024-12-04 16:27:41 +0000

    fix test output

--- test/body_mask_handling_test.py
+++ test/body_mask_handling_test.py
@@ -19,23 +19,18 @@ class TestSingleMaskHandling(unittest.TestCase):
     file_dir = os.path.dirname(os.path.realpath(__file__))
     test_image_base_path = os.path.abspath(os.path.join(file_dir, "leftImg8Bit"))
     test_mask_base_path = os.path.abspath(os.path.join(file_dir, "gtFine"))
+    image_mask_dict = dfa_utils.get_image_mask_dict(
+        test_image_base_path, test_mask_base_path, method="body"
+    )
+    output_dir = Path("/tmp/ldfa_tests")
 
     def test_(self):
-        json_files = dfa_io.glob_files_by_extension(self.test_mask_base_path, "json")
-        png_files = dfa_io.glob_files_by_extension(self.test_image_base_path, "png")
-        image_mask_dict = {}
-        image_mask_dict = dfa_utils.add_file_paths_to_image_mask_dict(
-            json_files, image_mask_dict, "mask_file"
-        )
-        image_mask_dict = dfa_utils.add_file_paths_to_image_mask_dict(
-            png_files, image_mask_dict, "image_file"
-        )
-        for i, entry in enumerate(image_mask_dict.values()):
+        for i, entry in enumerate(self.image_mask_dict.values()):
             image = dfa_utils.preprocess_image(entry["image_file"])
-            bodies = dfa_io.get_bodies_from_file(entry["mask_file"])
+            bodies = dfa_io.get_bodies_from_file(entry)
             bodies = add_body_cutout_and_mask_img(bodies, image)
             for j, body in enumerate(bodies):
-                body.save(Path("/temp"), i, j)
+                body.save(self.output_dir, i, j)
 
 
 if __name__ == "__main__":
