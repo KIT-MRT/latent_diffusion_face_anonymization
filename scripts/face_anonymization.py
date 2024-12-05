326a5ec975ff37e688deaef5c80bb71c9a2f1094
roesch <kevin.roesch@kit.edu>
2024-12-05 12:57:46 +0000

    adapt generalized changes

--- scripts/face_anonymization.py
+++ scripts/face_anonymization.py
@@ -9,7 +9,7 @@ from diffusion_face_anonymisation.io_functions import (
 )
 from diffusion_face_anonymisation.anonymization_functions import (
     define_anon_function,
-    anonymize_image,
+    anonymize_face_image,
 )
 
 # Set up logging
@@ -23,6 +23,7 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     image_dir, mask_dir, output_dir, anon_method = setup_parser_and_parse_args()
     anon_function = define_anon_function(anon_method)
+    assert anon_function is not None
 
     logger.info(
         f"Starting face anonymization with images from {image_dir}, masks from {mask_dir}, output to {output_dir}"
@@ -32,7 +33,7 @@ if __name__ == "__main__":
         os.makedirs(debug_dir)
         logger.info(f"Debug directory created at {debug_dir}")
 
-    image_mask_dict = dfa_utils.get_image_mask_dict(str(image_dir), str(mask_dir))
+    image_mask_dict = dfa_utils.get_image_mask_dict(str(image_dir), str(mask_dir), method="face")
     logger.info(f"Found {len(image_mask_dict)} images with corresponding masks.")
     logger.info("Starting to anonymize faces in images.")
 
@@ -40,5 +41,5 @@ if __name__ == "__main__":
         image_file = entry["image_file"]
         mask_file = entry["mask_file"]
         logger.info(f"Processing image {image_file} with mask {mask_file}")
-        anon_img = anonymize_image(image_file, mask_file, anon_function)
+        anon_img = anonymize_face_image(image_file, mask_file, anon_function)
         save_anon_image(anon_img, image_file, output_dir, anon_method)
